5caaf6a331df27cd8e68b8a3a828c0071fac416e
Santiago Berniz <sberniz@gmail.com>
2020-08-03 14:36:46 -0700

    final

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md","r") as file:
 
 setuptools.setup(
    name="lambdata-sberniz",
-   version= "0.0.7",
+   version= "0.0.8",
    author="sberniz",
    description="a collection of data science helper functions",
    long_desription=LONG_DESCRIPTION,
